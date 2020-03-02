4752d9dfbf836aab0d98ed42b608b7146f78d3c8
IAleks94 <a12344@mail.ru>
2020-03-02 16:16:45 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+  if (typeof n !== 'string') {
+    n =''+n;
+  }
+  let regExp = /[\d]{1,}/g;
+  let validStr = n.match(regExp)[0];
+  return +(validStr.split('').reverse().join(''));
+};
